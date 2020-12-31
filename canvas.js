34042bca1d8510898881181eb47008f6ea04670a
tobiasnf <tobiasnf@gmail.com>
2020-12-31 10:01:53 +0100

    .

--- canvas.js
+++ canvas.js
@@ -1,15 +1,17 @@
 
-import * as THREE from 'https://threejsfundamentals.org/threejs/resources/threejs/r122/build/three.module.js';
-import { OrbitControls } from 'https://threejsfundamentals.org/threejs/resources/threejs/r122/examples/jsm/controls/OrbitControls.js';
+import * as THREE from 'https://threejsfundamentals.org/threejs/resources/threejs/r122/build/three.module.js'
+import { OrbitControls } from 'https://threejsfundamentals.org/threejs/resources/threejs/r122/examples/jsm/controls/OrbitControls.js'
 
 const theCanvas = document.getElementById('theCanvas')
+const canvasWidth = getComputedStyle(theCanvas).getPropertyValue('--canvasWidth')
+const canvasHeight = getComputedStyle(theCanvas).getPropertyValue('--canvasHeight')
 
 //scene
 const scene = new THREE.Scene()
 scene.background = new THREE.Color('#9c9c9c')
 //camera
 const camera = new THREE.PerspectiveCamera(75, theCanvas.clientWidth / theCanvas.clientHeight, 0.1, 1000)
-camera.position.set(2, 2, 2)
+camera.position.set(-2, 2, 2)
 camera.lookAt(0, 0, 0)
 //render
 const renderer = new THREE.WebGLRenderer({
@@ -26,54 +28,55 @@ const controls = new OrbitControls(camera, theCanvas)
 controls.target.set(0, 0, 0)
 controls.update()
 //object(s)
-export let objects = {}
 export let turtle = []
+export let objects = {}
 export let geometry, edgeGeo, lines, material, cube, lineMat
-export const newBlock = (pos, blockColor, isTurtle) => {
+export const newBlock = (pos, blockColor, blockType) => {
     geometry = new THREE.BoxGeometry(1, 1, 1)
     edgeGeo = new THREE.EdgesGeometry(geometry)
-    lineMat = new THREE.LineBasicMaterial({
-        color: 'white',
-        linewidth: 50,
-    })
+    lineMat = new THREE.LineBasicMaterial({ color: 'white', })
     lines = new THREE.LineSegments(edgeGeo, lineMat);
     lines.position.set(pos.x, pos.y, pos.z)
     scene.add(lines)
+
     material = new THREE.MeshPhongMaterial({
         color: blockColor,
-        opacity: isTurtle ? 0.5 : 0.3,
+        opacity: 0.7,
         transparent: true,
     })
     cube = new THREE.Mesh(geometry, material)
     cube.position.set(pos.x, pos.y, pos.z)
     scene.add(cube)
-    isTurtle ? turtle = [cube, lines] : objects[`${pos.x} ${pos.y} ${pos.z}`] = [cube, lines]
+
+    if (blockType === 'turtle') {
+        turtle = [cube, lines]
+    } else {
+        objects[`${pos.x} ${pos.y} ${pos.z}`] = [cube, lines]
+    }
 }
 //remove objects
-export const removeBlock = (object) => {
-    for (let i = 0; i < 2; i++) {
-        object[i].geometry.dispose()
-        object[i].material.dispose()
-        scene.remove(object[i])
+export const removeBlock = (object, propName) => {
+    for (let i = 0; i < object[propName].length; i++) {
+        object[propName][i].geometry.dispose()
+        object[propName][i].material.dispose()
+        scene.remove(object[propName][i])
     }
     renderer.renderLists.dispose()
-    //delete object
+    delete object[propName]
 }
 newBlock({ x: 0, y: 0, z: 0 }, 'red', 'turtle')
-//newBlock({ x: 0, y: 1, z: 0 }, 'red')
+newBlock({ x: 1, y: 0, z: 0 }, 'green')
 
 //animate
 const animate = () => {
     requestAnimationFrame(animate)
     renderer.render(scene, camera)
-    // cube.rotation.x += 0.01
-    // cube.rotation.y += 0.01
 }
 animate()
 
 window.addEventListener('resize', () => {
-    theCanvas.style.height = '60vh'
-    theCanvas.style.width = '80vw'
+    theCanvas.style.height = canvasHeight
+    theCanvas.style.width = canvasWidth
     renderer.setSize(theCanvas.clientWidth, theCanvas.clientHeight)
     camera.aspect = theCanvas.clientWidth / theCanvas.clientHeight
     camera.updateProjectionMatrix()
--- client.js
+++ client.js
@@ -2,7 +2,7 @@
 import * as cv from './canvas.js'
 
 const socket = new WebSocket('ws://localhost:8080')
-let turtleData
+let tData
 let btnObjects = document.getElementById('movementButtons').getElementsByTagName("*")
 let btnNames = []
 let objCoords = {}
@@ -26,46 +26,52 @@ socket.addEventListener('close', () => {
     document.getElementById('status').innerHTML = 'status: disconnected'
 })
 
+btnNames.forEach(button => {
+    document.getElementById(button).addEventListener('click', () => {
+        socket.send('From control panel: ' + button)
+    })
+})
+
 socket.addEventListener('message', message => {
 
     if (message.data.includes("From turtle: ")) {
-        turtleData = message.data.replace("From turtle: ", "")
-        turtleData = JSON.parse(turtleData)
-        console.log(turtleData)
+        tData = message.data.replace("From turtle: ", "")
+        tData = JSON.parse(tData)
+        console.log(tData)
 
         //inspected object coordinates
-        objCoords.Up = { "x": turtleData.coord.x, "y": (turtleData.coord.y + 1), "z": turtleData.coord.z }
-        objCoords.Down = { "x": turtleData.coord.x, "y": (turtleData.coord.y - 1), "z": turtleData.coord.z }
+        objCoords.Up = { "x": tData.coord.x, "y": (tData.coord.y + 1), "z": tData.coord.z }
+        objCoords.Down = { "x": tData.coord.x, "y": (tData.coord.y - 1), "z": tData.coord.z }
 
-        if (turtleData.direction == "forward")
-            objCoords.Forward = { "x": turtleData.coord.x + 1, "y": turtleData.coord.y, "z": turtleData.coord.z }
-        else if (turtleData.direction == "left")
-            objCoords.Forward = { "x": turtleData.coord.x, "y": turtleData.coord.y, "z": turtleData.coord.z - 1 }
-        else if (turtleData.direction == "right")
-            objCoords.Forward = { "x": turtleData.coord.x, "y": turtleData.coord.y, "z": turtleData.coord.z + 1 }
-        else if (turtleData.direction == "back")
-            objCoords.Forward = { "x": turtleData.coord.x - 1, "y": turtleData.coord.y, "z": turtleData.coord.z }
+        if (tData.direction == "forward")
+            objCoords.Forward = { "x": tData.coord.x + 1, "y": tData.coord.y, "z": tData.coord.z }
+        else if (tData.direction == "left")
+            objCoords.Forward = { "x": tData.coord.x, "y": tData.coord.y, "z": tData.coord.z - 1 }
+        else if (tData.direction == "right")
+            objCoords.Forward = { "x": tData.coord.x, "y": tData.coord.y, "z": tData.coord.z + 1 }
+        else if (tData.direction == "back")
+            objCoords.Forward = { "x": tData.coord.x - 1, "y": tData.coord.y, "z": tData.coord.z }
 
         //move turtle to the new position
-        cv.turtle[0].position.set(turtleData.coord.x, turtleData.coord.y, turtleData.coord.z)
-        cv.turtle[1].position.set(turtleData.coord.x, turtleData.coord.y, turtleData.coord.z)
+        cv.turtle[0].position.set(tData.coord.x, tData.coord.y, tData.coord.z)
+        cv.turtle[1].position.set(tData.coord.x, tData.coord.y, tData.coord.z)
 
         //remove old objects
         const removeInspectedBlock = (blockCoords) => {
             if (cv.objects[`${blockCoords.x} ${blockCoords.y} ${blockCoords.z}`])
-                cv.removeBlock(cv.objects[`${blockCoords.x} ${blockCoords.y} ${blockCoords.z}`])
+                cv.removeBlock(cv.objects, [`${blockCoords.x} ${blockCoords.y} ${blockCoords.z}`])
         }
         removeInspectedBlock(objCoords.Down)
         removeInspectedBlock(objCoords.Forward)
         removeInspectedBlock(objCoords.Up)
-        removeInspectedBlock(turtleData.coord)
+        removeInspectedBlock(tData.coord)
 
         //place inspected blocks
-        if (turtleData.blocks.Down != "air")
+        if (tData.blocks.Down != "air")
             cv.newBlock({ x: objCoords.Down.x, y: objCoords.Down.y, z: objCoords.Down.z }, 'green')
-        if (turtleData.blocks.Forward != "air")
+        if (tData.blocks.Forward != "air")
             cv.newBlock({ x: objCoords.Forward.x, y: objCoords.Forward.y, z: objCoords.Forward.z }, 'green')
-        if (turtleData.blocks.Up != "air")
+        if (tData.blocks.Up != "air")
             cv.newBlock({ x: objCoords.Up.x, y: objCoords.Up.y, z: objCoords.Up.z }, 'green')
 
     } else {
@@ -73,13 +79,8 @@ socket.addEventListener('message', message => {
     }
 })
 
-btnNames.forEach(button => {
-    document.getElementById(button).addEventListener('click', () => {
-        socket.send('From control panel: ' + button)
-    })
-})
+
 
 // document.getElementById('status').addEventListener('click', () => {
 //     console.log(cv.objects)
-//     console.log(cv.objects.indexOf("1 0 0"))
 // })
